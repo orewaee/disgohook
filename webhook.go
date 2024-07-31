e70dfe1c170dd03558e0b804cf00900730f92290
orewaee <orewaee@gmail.com>
2024-07-31 15:13:52 +0400

    feat: added webhook name and avatar

--- webhook.go
+++ webhook.go
@@ -61,6 +61,96 @@ type Body struct {
 	Embeds  []*Embed `json:"embeds,omitempty"`
 }
 
+func (webhook *Webhook) SetName(name string) error {
+	body, err := json.Marshal(struct {
+		Name string `json:"name"`
+	}{name})
+	if err != nil {
+		return err
+	}
+
+	buffer := bytes.NewBuffer(body)
+	req, err := http.NewRequest(http.MethodPatch, webhook.url, buffer)
+	if err != nil {
+		return err
+	}
+
+	contentType := "application/json; charset=UTF-8"
+	req.Header.Set("Content-Type", contentType)
+
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := res.Body.Close(); err != nil {
+			return
+		}
+	}()
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	ok := res.StatusCode == http.StatusOK ||
+		res.StatusCode == http.StatusNoContent
+
+	if !ok {
+		return errors.New("failed to send webhook: " + string(resBody))
+	}
+
+	return nil
+}
+
+func (webhook *Webhook) SetAvatar(base64 string) error {
+	body, err := json.Marshal(struct {
+		Avatar string `json:"avatar"`
+	}{base64})
+	if err != nil {
+		return err
+	}
+
+	buffer := bytes.NewBuffer(body)
+	req, err := http.NewRequest(http.MethodPatch, webhook.url, buffer)
+	if err != nil {
+		return err
+	}
+
+	contentType := "application/json; charset=UTF-8"
+	req.Header.Set("Content-Type", contentType)
+
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := res.Body.Close(); err != nil {
+			return
+		}
+	}()
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	ok := res.StatusCode == http.StatusOK ||
+		res.StatusCode == http.StatusNoContent
+
+	if !ok {
+		return errors.New("failed to send webhook: " + string(resBody))
+	}
+
+	return nil
+}
+
 func (webhook *Webhook) Send() error {
 	body, err := json.Marshal(webhook.body)
 	if err != nil {
